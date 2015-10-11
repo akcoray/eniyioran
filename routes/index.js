855455921a9ac089daf8b70560190df836d78d96
fatih kiymet <fatih.kiymet@gmail.com>
2015-10-11 23:50:58 +0300

    design implementation

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 var constants = require('../lib/constants')
 var competitionService = require('../lib/competition-service')
 /* GET home page. */
-router.get('/', function(req, res) {	
+router.get('/', function(req, res) {  
   res.render('index', {
   	 	title: 'Express',
   	 	today : moment(Date.now()).format(constants.gridHeaderDateFormat),
