d51f549d140755720fc5fc5edf13cd711da87bdd
fatih kiymet <fatih.kiymet@gmail.com>
2015-10-20 00:46:44 +0300

    layout workouts

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,10 +5,11 @@ var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\
 
 function convertDateStringsToDates(input) {
     // Ignore things that aren't objects.
-    if (typeof input !== "object") return input;
+    if (typeof input !== "object") return input;    
 
     for (var key in input) {
         if (!input.hasOwnProperty(key)) continue;
+
 
         var value = input[key];
         var match;
@@ -23,6 +24,67 @@ function convertDateStringsToDates(input) {
             convertDateStringsToDates(value);
         }
    }
+}
+
+
+
+window.onload = function()
+{
+    var el = document.querySelector('.scroll-container');    
+    var handle = document.querySelector('.handle');
+    var mainMenu = document.querySelector('#main-menu');
+
+    var dragStart = false;
+    var start = 0;
+    var end = null;
+    var totalSize = 400;
+    var itemCount = 4;
+    var scrollSize = 200;
+    var scrollPortion = scrollSize / itemCount;
+
+    handle.addEventListener("transitionend", move, false)
+
+    function slide(endPos)
+    {
+        if(endPos > handle.offsetLeft)
+        {
+            handle.style.left = endPos - handle.offsetWidth + 'px';
+        }
+        else
+        {
+            handle.style.left = endPos + 'px';
+        }
+    }
+
+    function move()
+    {
+        debugger
+        var step = Math.round(handle.offsetLeft / 50);        
+        mainMenu.style.transform = 'translate(' + step * 100 * -1 + 'px, 0)';
+    }
+
+    el.onclick = function(e)
+    {        
+        slide(e.offsetX);      
+    }
+/*
+    el.onmousedown = function(e)
+    {
+        dragStart = true;
+    }
+
+    el.onmousemove = function(e)
+    {
+        if(dragStart)
+        {
+            slide(e.offsetX);
+        }
+    }
+
+    el.onmouseup = function(e)
+    {
+        dragStart = false;
+    }*/
 }
 
 var app = angular.module('BestBet', [])
