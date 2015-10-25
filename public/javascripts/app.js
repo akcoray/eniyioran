a6edf8b0ed4a3bf9fc6294ee6b46c7f7c9128855
fatih kiymet <fatih.kiymet@gmail.com>
2015-10-25 14:47:58 +0200

    menu

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -26,160 +26,127 @@ function convertDateStringsToDates(input) {
    }
 }
 
+function getLeft(element){
+    return element.getBoundingClientRect().left - element.offsetParent.getBoundingClientRect().left;
+} 
 
+function Slider(scrollContent)
+{    
+    var self = this;
 
-window.onload = function()
-{
-    var el = document.querySelector('.scroll-container');    
-    var handle = document.querySelector('.handle');
-    var mainMenu = document.querySelector('#main-menu');
-    var items = mainMenu.querySelectorAll('li')
+    this.slidee = scrollContent;
+    this.frame = scrollContent.offsetParent;
 
-    var pageSize = 4;
-    var totalStep = items.length - pageSize;
-    var scrollSize = el.offsetWidth - handle.offsetWidth;
-    var itemSize = items[0].offsetWidth;
-    var totalSize = items.length * itemSize;    
-    var scrollPortion = (scrollSize / (totalStep * itemSize)) *  itemSize;
-    console.log('scrollPortion ' + scrollPortion); 
-    var _dragStart = false;
-    var _touchStart = false;
-    var _touchStartX = 0;
-    var touchEndX = 0;
-    var forward = 1;
-    var currentStep = 0;
+    var items = document.querySelectorAll('li');
+    this.scrollLeft = 0;
 
-    Object.defineProperty(this, 'dragStart', 
+    //field keeps where user touched at start
+    self.touchStartX = 0;    
+
+    self.touchEndX = 0;
+
+    //flags keeps touch has started
+    //it is used to check if an active touch exists
+    self.touchStarted = false;
+
+    //contentSize
+    Object.defineProperty(self, 'contentSize', {
+        set : function(value) {
+            this.slidee.style.width = value + 'px';
+        },
+        get : function(){
+            return this.slidee.offsetWidth;
+        }
+    });
+
+    //frameSize
+    Object.defineProperty(self, 'frameSize', {
+        set : function(value){
+            this.frame.style.width = value + px;
+        },
+        get : function(){
+            return this.frame.offsetWidth;
+        }
+    });
+
+    //scrollSize
+    Object.defineProperty(self, '_scrollSize', {
+        writable : false,
+        value  : this.slidee.offsetWidth - this.frame.offsetWidth
+    });
+
+    //field keeps elements left position
+    Object.defineProperty(self, 'left', {
+        set : function(value){
+            console.log("slidee width " +   self.slidee.offsetWidth);
+            console.log("frame width " +   self.frame.offsetWidth);
+            console.log(value);
+            if(value < 0 && value > ( -1* self._scrollSize) || value > 0 && self.left < 0)
+                this.slidee.style.transform =  'translate(' +  value + 'px, 0)';
+            
+        },
+        get : function()
         {
-            get : function() { return _dragStart; },
-            set : function(value) {
-                _dragStart = value;
-            }
+            return getLeft(this.slidee);
         }
-     );
+    });
 
-    handle.addEventListener("transitionend", move);
-    mainMenu.addEventListener("transitionend", reset);
-
-    //number of steps to slide
-    function slide(step)
-    {
-        if(currentStep + step < 0 || currentStep + step > totalStep)
-        {        
-            console.log("can't walk");
-            reset();
-            return;
-        }
-
-        currentStep = currentStep + step; 
-        console.log("step " + currentStep);
-        handle.style.transform = 'translate(' +  currentStep * scrollPortion + 'px, 0)';
-    }
-
-    function move()
-    { 
-        console.log("slide transitionend");                
-        mainMenu.style.transform = 'translate(' + -1 * currentStep * itemSize + 'px, 0)';
-    }
-
-    function getLeft(element){
-        return  Math.round(element.getBoundingClientRect().left - el.getBoundingClientRect().left);
-    } 
-
-    function reset(){
-        _touchStartX = 0;
-        _touchStart = false;
+    self.reset = function(){
+        self.touchStartX = 0;
+        self.touchStarted = false;
         console.log("reset");
     }
 
-    mainMenu.addEventListener('touchstart', function(e) {  
+
+    //touch started event
+    self.slidee.addEventListener('touchstart', function(e) {  
         console.log("touch started");        
-        if(!_touchStart)
+        if(!self.touchStarted && event.targetTouches.length == 1)
         {
-            if (event.targetTouches.length == 1) {
-                    var touch = event.targetTouches[0];
-                    _touchStart = true;
-                    _touchStartX = touch.pageX;
-                    console.log("touch start X " + touch.pageX);
-            }
+            var touch = event.targetTouches[0];
+            self.touchStarted = true;
+            self.touchStartX = touch.pageX;
+            console.log("touch start X " + self.touchStartX);                  
         }
     }, false);
 
-    mainMenu.addEventListener('touchend', function(e) {               
+    //touch started event
+    self.slidee.addEventListener('touchend', function(e) {               
         console.log("touch end");           
-        if(_touchStart)
-            {
+        if(self.touchStarted)
+        {
             if (event.changedTouches.length == 1) {
                 var touch = event.changedTouches[0];
-                console.log("touch end touch start point " + _touchStartX);
-                if(touch.pageX - _touchStartX > 0)                
-                    slide(-1);
-                else
-                    slide(1);            
+                console.log("touch end touch start point " + self.touchStartX);
+                self.reset();
             }
-            else
-            {
-                reset();
+            else{
+                console.log("page X " + touch.pageX);
+                self.reset();
             }
         }    
     }, false);
 
-    mainMenu.addEventListener('touchmove', function(e) {        
-        e.preventDefault();
+    //touch started event
+    self.slidee.addEventListener('touchmove', function(e) {        
+        e.preventDefault();        
         console.log("touchmove");        
+        if (event.changedTouches.length == 1) {
+            var touch = event.changedTouches[0];
+            var distance = (touch.pageX - self.touchStartX);
+            console.log("how much left it pulled " + distance);
+            self.left = self.left + distance;
+            self.touchStartX += distance;            
+        }
 
     }, false);
-
-    el.onclick = function(e)
-    {        
-        var distance = getLeft(handle) - e.offsetX;
-
-        /*if(distance / )
-
-        slide();      */
-    }
+}
 
 
-  /*  el.onmousemove = function(e)
-    {
-        if(dragStart)
-        {
-            handle.style.left = e.offsetX + 'px';
-        }
-    }*/
+window.onload = function()
+{
+    var slider = new Slider(document.querySelector('#main-menu'));
 
-    el.onmouseup = function(e)
-    {
-        dragStart = false;
-        slide(e.offsetX);
-    }
-
-    handle.onclick = function(e)
-    {
-        e.stopPropagation();
-    }
-
-    handle.onmousedown = function(e)
-    {
-        e.stopPropagation();
-        dragStart = true;
-    }
-
-    handle.onmouseup = function(e)
-    {
-        e.stopPropagation();
-        dragStart = false;     
-    }
-
-    handle.onmousemove = function(e){
-        e.stopPropagation();    
-    }
-
-    document.onmouseup = function(e)
-    {
-        dragStart = false;
-        return false;
-    }
 }
 
 var app = angular.module('BestBet', [])
